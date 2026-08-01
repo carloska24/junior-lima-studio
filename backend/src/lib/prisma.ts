32c100b27052cb7a5bf19e3f9d02b44586d8a2fa
carloska24 <carloska24@gmail.com>
2026-07-31 21:11:29 -0300

    Use native Prisma Client to fix connection on Render

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,14 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 require('dotenv').config();
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ 
-  connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined
-});
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
